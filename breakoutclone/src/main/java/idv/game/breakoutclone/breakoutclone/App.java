7e0b8981830bcf8db42a5218c49a2c23e439f50e
Kuanwei <Kuanwei@DESKTOP-Q3LU8R3>
2023-01-06 11:45:26 +0800

    init

---
+++ breakoutclone/src/main/java/idv/game/breakoutclone/breakoutclone/App.java
@@ -0,0 +1,13 @@
+package idv.game.breakoutclone.breakoutclone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
